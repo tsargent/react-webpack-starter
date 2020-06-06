78a221e27f4d41a249189c56c0de6310721e40ec
Tyler Sargent <tsargent@gmail.com>
2020-06-06 17:07:11 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+const App = () => (
+  <div className="m-8">
+    <h1 className="text-2xl">SWAPI App</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    "main": path.join(__dirname, "src/main.js")
+  },  
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
+      },
+    ],  
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ]
+}
