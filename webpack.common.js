8850bd8cbe3d7d5df06d62ba156f5947b7c7e93a
Tyler Sargent <tsargent@gmail.com>
2020-06-06 22:10:27 -0400

    fixing build

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
     "main": path.join(__dirname, "src/main.js")
-  },  
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "./dist")
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist")
   },
