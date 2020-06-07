1c81950a5bb6929eddef6557af3de552ce06aa39
Tyler Sargent <tsargent@gmail.com>
2020-06-06 22:14:06 -0400

    fixing build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,12 @@
 const merge = require("webpack-merge");
+const path = require("path");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "[name].js"
+    path: path.resolve('./dist'),
+    filename: "[name].js",
+    publicPath: '/',
   },
 });
